33681a26a12a88f8a36a2cc14172c248969ee0d3
Samudra <wadugesamudra@gmail.com>
2025-09-24 20:47:24 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import rewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), rewriteAll()],
   build: {
     sourcemap: true, // Enable source maps in production build
   },
